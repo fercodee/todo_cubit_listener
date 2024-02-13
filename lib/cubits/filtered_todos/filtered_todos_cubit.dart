95b28b269e59b54ee0c16b37adaf772f8774e78b
fercodee <devbyfull@gmail.com>
2024-02-13 20:43:20 -0300

    cubit and BlocListener

---
+++ lib/cubits/filtered_todos/filtered_todos_cubit.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../models/todo_model.dart';
+
+part 'filtered_todos_state.dart';
+
+class FilteredTodosCubit extends Cubit<FilteredTodosState> {
+  final List<Todo> initialTodos;
+
+  FilteredTodosCubit({
+    required this.initialTodos,
+  }) : super(FilteredTodosState(filteredTodos: initialTodos));
+
+  void setFilteredTodos(Filter filter, List<Todo> todos, String searchTerm) {
+    List<Todo> filteredTodos;
+
+    switch (filter) {
+      case Filter.active:
+        filteredTodos = todos.where((Todo todo) => !todo.completed).toList();
+        break;
+      case Filter.completed:
+        filteredTodos = todos.where((Todo todo) => todo.completed).toList();
+        break;
+      default:
+        filteredTodos = todos;
+        break;
+    }
+
+    if (searchTerm.isNotEmpty) {
+      filteredTodos = filteredTodos
+          .where((Todo todo) => todo.desc.toLowerCase().contains(searchTerm))
+          .toList();
+    }
+
+    emit(state.copyWith(filteredTodos: filteredTodos));
+  }
+}
